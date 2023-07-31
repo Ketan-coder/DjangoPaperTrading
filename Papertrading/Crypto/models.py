343656f684c7024e01e81204edc5ad827e0745a1
ketan <you@example.com>
2023-07-31 12:37:37 +0530

    Added Crypto App

---
+++ Papertrading/Crypto/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Crypto(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10, unique=True)
+    lastPrice = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.symbol} - Balance: {self.lastPrice}"
